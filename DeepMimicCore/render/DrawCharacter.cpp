419afd36f2192578dd3c133e4ecff32ee6a95925
范锦昌 <jackyfan@moonton.com>
2021-10-26 19:58:53 +0800

    日志

--- DeepMimicCore/render/DrawCharacter.cpp
+++ DeepMimicCore/render/DrawCharacter.cpp
@@ -107,6 +107,16 @@ void cDrawCharacter::DrawCharShapes(const cCharacter& character, const tVector& 
 		int parent_joint = cKinTree::GetDrawShapeParentJoint(curr_def);
 		tMatrix parent_world_trans = character.BuildJointWorldTrans(parent_joint);
 
+		/*parent_world_trans << 0.999801, -0.0010596, 0.0199322, 0.0119301,
+			0.00111006, 0.999996, -0.00252073, 0.0499361,
+			-0.0199294, 0.00254235, 0.999798, 0.240212,
+			0, 0, 0, 1;*/
+
+		parent_world_trans << 0.995075, -0.00033007, -0.0991241, 0.219024,
+			0.000338421, 1, 6.74357e-05, 0.0499651,
+			0.099124, -0.000100649, 0.995075, 0.258042,
+			0, 0, 0, 1;
+
 		int mesh_id = cKinTree::GetDrawShapeMeshID(curr_def);
 		if (mesh_id != gInvalidIdx)
 		{
@@ -129,6 +139,7 @@ void cDrawCharacter::DrawCharShapes(const cCharacter& character, const tVector& 
 			{
 				continue;
 			}
+			std::cout << "---DrawCharShapes parent_world_trans:" << parent_world_trans << std::endl;
 			cDrawCharacter::DrawShape(curr_def, parent_world_trans, fill_tint, line_col);
 		}
 	}
@@ -178,13 +189,15 @@ void cDrawCharacter::DrawShapeBox(const cKinTree::tDrawShapeDef& def, const tMat
 	cDrawUtil::Rotate(euler);*/
 
 	cDrawUtil::SetColor(col);
+	//这里的DrawBox其实只是调用了 DrawUtil的 gBoxSolidMesh->Draw(GL_TRIANGLES);
 	cDrawUtil::DrawBox(tVector::Zero(), size, cDrawUtil::eDrawSolid);
+	
 
-	if (line_col[3] > 0)
+	/*if (line_col[3] > 0)
 	{
 		cDrawUtil::SetColor(line_col);
 		cDrawUtil::DrawBox(tVector::Zero(), size, cDrawUtil::eDrawWireSimple);
-	}
+	}*/
 
 	cDrawUtil::PopMatrixView();
 }
